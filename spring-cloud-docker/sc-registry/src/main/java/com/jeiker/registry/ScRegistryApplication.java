2232a65bdfef61b02d3e01029f3c319052a06d7d
jeikerxiao <jeiker@126.com>
2017-12-27 10:23:47 +0800

    change sc-registry
    change sc-registry

--- spring-cloud-docker/sc-registry/src/main/java/com/jeiker/registry/ScRegistryApplication.java
+++ spring-cloud-docker/sc-registry/src/main/java/com/jeiker/registry/ScRegistryApplication.java
@@ -2,8 +2,10 @@ package com.jeiker.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ScRegistryApplication {
 
 	public static void main(String[] args) {
