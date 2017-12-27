c3dac5ed7ed223139547e2a7bc8c27c5a9c9d9bb
jeikerxiao <jeiker@126.com>
2017-12-27 11:20:56 +0800

    change project config
    change project config

--- spring-cloud-docker/sc-zipkin/src/main/java/com/jeiker/zipkin/ScZipkinApplication.java
+++ spring-cloud-docker/sc-zipkin/src/main/java/com/jeiker/zipkin/ScZipkinApplication.java
@@ -2,8 +2,10 @@ package com.jeiker.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
+@EnableZipkinStreamServer
 public class ScZipkinApplication {
 
 	public static void main(String[] args) {
